6104722138caa090413bda8d8ad5a7112847ffe5
TheAdaptoid <mhwkmn22@gmail.com>
2023-11-01 23:09:57 -0400

    first commit

---
+++ Client.java
@@ -0,0 +1,119 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Client {
+    
+    public static void main(String[] args) throws InterruptedException {
+        //Setup and prep
+        System.out.print("Booting Client...");
+        Scanner userInput = new Scanner(System.in);
+        int userCommand = 0;
+
+        //Get server IP
+        System.out.print("\nEnter server IP or Network address: ");
+        String serverIP = userInput.nextLine();
+
+        //Get server port
+        System.out.print("Enter server port. Range 1025-4998: ");
+        int serverPort = userInput.nextInt();
+
+        while (true) {
+            //Get user command
+            System.out.print(
+            "\nSelect a command:" + 
+            "\n0 - Date/Time" +
+            "\n1 - Uptime" +
+            "\n2 - Memory Usage" +
+            "\n3 - Network Connections" +
+            "\n4 - User List" +
+            "\n5 - Process List" +
+            "\n6 - exit" +
+            "\nEnter command (int): ");
+            userCommand = userInput.nextInt();
+            
+            //Exit case
+            if (userCommand == 6) {
+                break;
+            }
+
+            //Get number of requests to generate
+            System.out.print(
+            "\nSelect the number of requests to generate:" +
+            "\n 1, 5, 10, 15, 20, 25" +
+            "\nEnter number of requests (int): ");
+            int requestCount = userInput.nextInt();
+
+            //Generate threaded requests
+            ArrayList<Thread> threadsArray = new ArrayList<>();
+            ArrayList<Long> requestTimes = new ArrayList<>();
+            for (int i = 0; i < requestCount; i++) {
+                final int localUserCommand = userCommand;
+                Thread thread = new Thread(() -> CreateRequest(serverIP, serverPort, localUserCommand));
+                threadsArray.add(thread);
+                thread.start();
+                requestTimes.add(System.currentTimeMillis());
+            }
+
+            //Collect all threads
+            for (Thread thread : threadsArray) {
+                thread.join();
+                long calculatedTime = System.currentTimeMillis() - requestTimes.get(threadsArray.indexOf(thread));
+                requestTimes.set(threadsArray.indexOf(thread), calculatedTime);
+            }
+
+            //Calculate thread times
+            long totalThreadTime = 0;
+            for (long time : requestTimes) {
+                totalThreadTime += time;
+            }
+            float averageThreadTime = totalThreadTime / requestCount;
+            System.out.println("Average thread time: " + averageThreadTime + " ms");
+            System.out.println("Total thread time: " + totalThreadTime + " ms");
+
+        }
+
+        //Close client socket
+        System.out.println("Closing client socket...");
+        userInput.close();
+    }
+
+    private static void CreateRequest(String serverIP, int serverPort, int dataRequest) {
+        String serverResponse = "No response from server.";
+        
+        try {
+            //Create client socket
+            Socket clientSocket = new Socket(serverIP, serverPort);
+
+            //Send data request to server
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            writer.println(dataRequest);
+
+            //Get response from server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            serverResponse = "";
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                serverResponse += line + "\n";
+            }
+
+            //Report response
+            System.out.println(serverResponse);
+
+            //Close client socket
+            clientSocket.close();
+
+        } catch (UnknownHostException e) {
+            System.out.println("Error occurred. Could not create client socket.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error occurred. Could not create client socket.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,142 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Interative_Server
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        //Setup and prep
+        System.out.println("Booting Server...");
+        Scanner userInput = new Scanner(System.in);
+        Process process = null;
+        BufferedReader processOut = null;
+        String currentStreamline = "";
+
+        //Get port to observe
+        System.out.println("Enter port to observe. Range 1025-4998: ");
+        int observedPort = userInput.nextInt();
+
+        try {
+            //Create socket server
+            System.out.println("Creating socket for port " + observedPort + "...");
+            ServerSocket serverSocket = new ServerSocket(observedPort);
+            System.out.println("Socket created.");
+
+            while (true) {
+                //Take in input from observed port
+                System.out.println("Waiting for client requests...");
+                Socket currentRequest = serverSocket.accept();
+                long requestStartTime = System.currentTimeMillis();
+                String requestClientInfo = currentRequest.getInetAddress().getHostAddress() + ":" + currentRequest.getPort();
+                
+                //Determine data request type
+                BufferedReader reader = new BufferedReader(new InputStreamReader(currentRequest.getInputStream()));
+                int dataRequest = Integer.parseInt(reader.readLine());
+
+                String requestResponse = "";
+                switch (dataRequest) {
+                    case 0:
+                        //DateTime request
+                        System.out.println("Date and time request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"date"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Date/Time: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                        break;
+                    case 1:
+                        //Uptime request
+                        System.out.println("Uptime request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"uptime"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Uptime: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                        break;
+                    case 2:
+                        //Memory request
+                        System.out.println("Memory usage request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"free", "-g"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Memory Usage: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                        break;
+                    case 3:
+                        //Netstat request
+                        System.out.println("Netstat request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"netstat"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Network Connections: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                        break;
+                    case 4:
+                        //User list request
+                        System.out.println("User list request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"who"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Current Users: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                        break;
+                    case 5:
+                        //Process list request
+                        System.out.println("Process list request received from: " + requestClientInfo);
+                        process = Runtime.getRuntime().exec(args = new String[]{"ps"});
+                        processOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        requestResponse = "Current Processes: \n";
+                        currentStreamline = "";
+                        while ((currentStreamline = processOut.readLine()) != null) {
+                            requestResponse += currentStreamline + "\n";
+                        }
+                        processOut.close();
+                    default:
+                        //Invalid request
+                        System.out.println("Invalid request received from: " + requestClientInfo);
+                        requestResponse = "Invalid request received.";
+                        break;
+                }
+
+                //Send response
+                PrintWriter writer = new PrintWriter(currentRequest.getOutputStream(), true);
+                writer.println(requestResponse);
+
+                //Close request
+                currentRequest.close();
+
+                //Report response time
+                long requestEndTime = System.currentTimeMillis() - requestStartTime;
+                System.out.println("Response time: " + requestEndTime + " ms");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error occurred. Could not create socket.");
+            e.printStackTrace();
+        }
+
+        userInput.close();
+    }
+}
